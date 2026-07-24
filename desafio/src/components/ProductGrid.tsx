a48253880d49372a0072632adfb818a4623d145f
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2026-07-24 13:10:32 -0600

    SearcBar hecho por - MO232723

--- desafio/src/components/ProductGrid.tsx
+++ desafio/src/components/ProductGrid.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { Product } from "../types/Product";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { books } from "../data/data";
 import { addToCart } from "../redux/cartSlice";
 import { useAppDispatch } from "../redux/hooks";
 
 import "../styles/product-grid.css";
 import BookModal from "./Modal";
+// SearchBar
+import SearchBar from "../components/SearchBar";
+
 
 
 export default function ProductGrid() {
@@ -15,9 +18,36 @@ export default function ProductGrid() {
     const [selectedBook, setSelectedBook] =
         useState<Product | null>(null);
 
+    const [query, setQuery] = useState("");
+
+    /*
+    El buscador solo mostrara el titulo y este sera sencillo a las mayus/minus 
+
+    Si esta vacio se mostrara el catalogo completo de todos los libros
+    */
+    const filteredBooks = useMemo(() => {
+        const libN = query.trim().toLowerCase();
+        // si no lo encuentra muestra todos los libros
+        if(!libN) return books;
+        return books.filter((book) => book.title.toLowerCase().includes(libN));
+    }, [query]);
+
     return (
+        /* Seccion del buscador */
+        <section className="product-section">
+            <SearchBar onSearch={setQuery} />
+
+            <p className="product-count">
+                {filteredBooks.length} de {books.length} libros
+            </p>
+
+            {filteredBooks.length === 0 ? (
+                /* Si no encuentra no muestra nada */
+                <p className="product-empty">Sin Coincidencias para "{query}".</p>
+            ): (
+      
         <div className="product-grid">
-            {books.map((book) => (
+            {filteredBooks.map((book) => (
                 <div key={book.id}
                     className="product-card"
                 >
@@ -53,6 +83,7 @@ export default function ProductGrid() {
                 onClose={() => setSelectedBook(null)}
             />
         </div>
-
+    )}
+      </section>
     );
 }
